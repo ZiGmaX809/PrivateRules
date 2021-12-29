d50f257c26d4c73c0baae8815ddd7ee09e417437
ZiGmaX809 <pyrogas7@gmail.com>
2021-12-29 11:16:10 +0800

    Update and rename test.js to answer.js

---
+++ QuantumultX/answer.js
@@ -0,0 +1,27 @@
+/*
+之江杯答案
+
+**************************/
+let obj = JSON.parse($response.body || '{}');
+var title = [];
+var id = [];
+
+if (obj.code == 3001) {
+    for (i = 0; i < 10; i++) {
+        //title.push(obj.data.list[i].title);
+        id.push("[" + obj.data.list[i].win_id + "]");
+        //$notify("之江杯答案", obj.data.list[i].win_id, obj.data.list[i].title);
+        //delay(3);
+    }
+    //$notify("之江杯答案", "", list);
+    //console.log(id.toString());
+    $notify("之江杯答案", "", id.toString());
+}
+
+function delay(n) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, n * 1000);
+    });
+}
+
+$done();
