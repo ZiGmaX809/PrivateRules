a2018ebc8943b4eff791e7835a7903425f602a4e
ZiGmaX809 <pyrogas7@gmail.com>
2021-12-29 14:21:48 +0800

    Update answer.js

--- QuantumultX/answer.js
+++ QuantumultX/answer.js
@@ -5,11 +5,12 @@
 let obj = JSON.parse($response.body || '{}');
 var title = [];
 var id = [];
+var num = [①,②,③,④,⑤,⑥,⑦,⑧,⑨,⑩]
 
 if (obj.code == 3001) {
     for (i = 0; i < 10; i++) {
         //title.push(obj.data.list[i].title);
-        id.push("【" + (i + 1) + "# " + obj.data.list[i].win_id + "】");
+        id.push("[" + num[i] + obj.data.list[i].win_id + "]");
         //$notify("之江杯答案", obj.data.list[i].win_id, obj.data.list[i].title);
         //delay(3);
     }
