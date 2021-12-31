c5f2c99d51077d4542d594bf00fc3c75519c1cf7
ZiGmaX809 <pyrogas7@gmail.com>
2021-12-31 10:47:32 +0800

    Create final.js

---
+++ QuantumultX/Scripts/PushAnswer/final.js
@@ -0,0 +1,27 @@
+/*
+之江杯答案
+**************************/
+let obj = JSON.parse($response.body || '{}');
+var title = [];
+var id = [];
+var num = ['①','②','③','④','⑤','⑥','⑦','⑧','⑨','⑩']
+
+if (obj.code == 3001) {
+    for (i = 0; i < 10; i++) {
+        //title.push(obj.data.list[i].title);
+        id.push(num[i] + "[" + obj.data.list[i].win_id + "]");
+        //$notify("之江杯答案", obj.data.list[i].win_id, obj.data.list[i].title);
+        //delay(3);
+    }
+    //$notify("之江杯答案", "", list);
+    //console.log(id.toString());
+    $notify("之江杯答案", "", id.toString());
+}
+
+function delay(n) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, n * 1000);
+    });
+}
+
+$done();
